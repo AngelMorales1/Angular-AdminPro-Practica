dd27f0fb370a005f084df8765eaf1ba92de3fa68
nombre <mail@correo.com>
2020-12-29 02:31:36 -0300

    Modulos implementados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+
 import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+
+
 
 const routes: Routes = [
-  {path: '', 
-   component: PagesComponent,
-   children: [
-    {path: 'dashboard', component: DashboardComponent },
-    {path: 'progress', component: ProgressComponent},
-    {path: 'grafica1', component: Grafica1Component},
-    {path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-   ]
-  },
 
-
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},  
+  // path: '/dashboard' PagesRouting
+  // path: '/auth' AuthRouting
 
   {path: '**', component: NoPageFoundComponent}
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
